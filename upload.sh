9ff687bbe9d86d1225e31fcfcb7e39f6a5a7bd50
kruz <kruz@ecwid.com>
2020-05-25 11:06:17 +0400

    to git hub

--- upload.sh
+++ upload.sh
@@ -3,4 +3,5 @@
 # закинуть файл в lamp
 #scp ./*.jpg kruz@lamp.ecwid.com:/home/kruz/public_html/images
 scp ./*.html kruz@lamp.ecwid.com:/home/kruz/public_html/cookie
+scp ./*.mp4 kruz@lamp.ecwid.com:/home/kruz/public_html/cookie
 
