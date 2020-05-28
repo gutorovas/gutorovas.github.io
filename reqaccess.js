1e4c407ec5a80bfd65ba449836801377eef48c8a
kruz <kruz@ecwid.com>
2020-05-28 08:17:38 +0400

    to git hub

--- reqaccess.js
+++ reqaccess.js
@@ -26,3 +26,34 @@ function reqAccess() {
         console.log('Browser has no hasStorageAccess or requestStorageAccess methods\n');
     }
 }
+
+function reqAccess_ecwid() {
+    if (document.hasStorageAccess && document.requestStorageAccess) {
+        console.log('Browser has hasStorageAccess and requestStorageAccess methods\n');
+        document.hasStorageAccess().then(
+            function successful(hasAccess) {
+                if (hasAccess) {
+                    console.log('Checking if hasAccess already granted.... granted already. Exit\n');
+                    window.location.replace("https://my.ecwid.com");
+                } else {
+                    console.log('Checking if hasAccess already granted.... not granted yet. Requesting...\n');
+                    document.requestStorageAccess().then(
+                        function successful() {
+                            console.log('Access request was a success\n');
+                            window.location.reload();
+                        },
+                        function fail() {
+                            console.log('Storage Access API call failed\n');
+                            window.open("https://my.ecwid.com", 'mywin',
+                                'toolbar=0,menubar=0,scrollbars=1,height=600,width=720');
+                        }
+                    );
+                }
+            },
+            function rejected(reason) {
+                console.log('hasStorageAccess failed: ' + reason + '\n');
+            });
+    } else {
+        console.log('Browser has no hasStorageAccess or requestStorageAccess methods\n');
+    }
+}
