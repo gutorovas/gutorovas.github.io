43da4c9225ff18247648302a18b65527d25f2d60
kruz <kruz@ecwid.com>
2020-05-28 08:24:34 +0400

    to git hub

--- reqaccess.js
+++ reqaccess.js
@@ -43,7 +43,7 @@ function reqAccess_ecwid() {
                             window.location.reload();
                         },
                         function fail() {
-                            console.log('Storage Access API call failed2\n');
+                            console.log('Storage Access API call failed\n');
                             window.open("https://my.ecwid.com", 'mywin',
                                 'toolbar=0,menubar=0,scrollbars=1,height=600,width=720');
                         }
