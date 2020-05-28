a25eda2be27495e98d13ca183d9819b75497ebd7
kruz <kruz@ecwid.com>
2020-05-28 09:25:24 +0400

    to git hub

--- reqaccess.js
+++ reqaccess.js
@@ -46,8 +46,9 @@ function reqAccess_ecwid() {
                         },
                         function fail() {
                             console.log('Storage Access API call failed\n');
-                            window.parent.open("https://gutorovas.github.io/redirect_page.html", 'mywin',
+                            window.top.open("https://gutorovas.github.io/redirect_page.html", 'mywin',
                                 'toolbar=0,menubar=0,scrollbars=1,height=600,width=720');
+                            window.top.postMessage('fuck', '*')
                         }
                     );
                 }
